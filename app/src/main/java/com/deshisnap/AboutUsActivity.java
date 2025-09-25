d72aa8a3a3647442d055477064b36e817e73d29d
Deshisnap <deshisnap@gmail.com>
2025-09-25 22:08:21 +0530

    Update AboutUsActivity.java

--- app/src/main/java/com/deshisnap/AboutUsActivity.java
+++ app/src/main/java/com/deshisnap/AboutUsActivity.java
@@ -2,7 +2,10 @@ package com.deshisnap;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.deshisnap.Utils;
+
 
 public class AboutUsActivity extends AppCompatActivity {
     @Override
@@ -10,10 +13,29 @@ public class AboutUsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
-        TextView title = findViewById(R.id.about_title);
-        TextView body = findViewById(R.id.about_body);
+        
+        // gradiant colour
+        TextView company_title= findViewById(R.id.company_title);
+        Utils.applyGradientToText(company_title, "#04FDAA", "#01D3F8");
 
-        title.setText("About DeshiSnap");
-        body.setText("DeshiSnap is your trusted home services partner. We connect you with verified professionals across plumbing, electrical, cleaning, and more — quickly and reliably.\n\nOur mission is to simplify your daily life with transparent pricing, quality service, and easy bookings.\n\nContact us: deshisnap247service@gmail.com");
-    }
+
+        TextView company_location = findViewById(R.id.company_location);
+        Utils.applyGradientToText(company_location, "#04FDAA", "#01D3F8");
+
+
+        TextView company_desc = findViewById(R.id.company_desc);
+        Utils.applyGradientToText(company_desc, "#04FDAA", "#01D3F8");
+
+        TextView company_supportgmail = findViewById(R.id.company_supportgmail);
+        Utils.applyGradientToText(company_supportgmail, "#04FDAA", "#01D3F8");
+
+             // ✅ Back button text
+        ImageView backText = findViewById(R.id.back_button);
+        backText.setOnClickListener(v -> {
+            onBackPressed(); // Go back to previous activity
+        }); // Go back to previous activity
+        }
+
+       
+    
 }
